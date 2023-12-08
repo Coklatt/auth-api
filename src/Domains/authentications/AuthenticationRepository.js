df6e24fc1c106ee8b40426e8916f78470a239684
reader00 <qoryamanahputra0708@gmail.com>
2023-12-08 15:46:14 +0700

    multi deploy s1-4 sample-1

--- src/Domains/authentications/AuthenticationRepository.js
+++ src/Domains/authentications/AuthenticationRepository.js
@@ -1,15 +1,15 @@
 class AuthenticationRepository {
-  async addToken(token) {
-    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
+    async addToken(token) {
+        throw new Error('AUTHENTICATION_REPOSITORY.ADD_TOKEN.METHOD_NOT_IMPLEMENTED');
+    }
 
-  async checkAvailabilityToken(token) {
-    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
+    async checkAvailabilityToken(token) {
+        throw new Error('AUTHENTICATION_REPOSITORY.CHECK_AVAILABILITY.METHOD_NOT_IMPLEMENTED');
+    }
 
-  async deleteToken(token) {
-    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
+    async deleteToken(token) {
+        throw new Error('AUTHENTICATION_REPOSITORY.DELETE_TOKEN.METHOD_NOT_IMPLEMENTED');
+    }
 }
 
 module.exports = AuthenticationRepository;
